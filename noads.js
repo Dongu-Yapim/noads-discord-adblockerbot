fc3cb6c95d4143c40e0b7d5d2537ff4b5a8f5a8d
Ripbyrustavi <murathalitcan@gmail.com>
2017-10-07 21:12:59 +0300

    Add files via upload

---
+++ noads.js
@@ -0,0 +1,58 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+var prefix = "noads";
+
+
+
+//////////////////////////////////////
+//##################################//
+//#                                #//
+//#         Anti ads bot 20        #//
+//#        Ripbyrustavi#8091       #//
+//#         DISCORD v12            #//
+//#                                #//
+//##################################//
+//////////////////////////////////////
+
+
+
+
+
+client.on('ready', () => {
+  console.log(`Giriþ yapýldý. Nick: ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'yardým') {
+    msg.reply('Yardýma ihtiyacýn yok. Otomatik olarak bot discord davet linklerini engeller.');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", msg => {
+  if (msg.content.toLowerCase().match(/(discord\.gg\/)|(discordapp\.com\/invite\/)/g) && !msg.author.bot && msg.channel.type === "text" && msg.channel.permissionsFor(msg.guild.member(client.user)).has("MANAGE_MESSAGES")) {
+    msg.delete(30).then(deletedMsg => {
+      deletedMsg.reply("Discord davet linki paylaþtýðýný algýladýk. Bu sunucu No-Ads ile korunmakta.").catch(e => {
+        console.error(e);
+      });
+    }).catch(e => {
+      console.error(e);
+    });
+  }
+});
+
+
+
+
+
+client.login('tokeninburayagelecek!');
